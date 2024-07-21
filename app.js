e5702c87da4fce9eaed1c163baecd2385c4a9219
yatheendrasai1 <yatheendrasai1@gmail.com>
2024-07-21 22:48:50 +0530

    BEBANTU-2: Added UI URL for CORS

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client/dist/client')));
 
 const corsOptions = {
-    origin: 'http://localhost:4200', // Allow requests from localhost:4200
+    origin: 'https://bantu-listen.vercel.app', // Allow requests from localhost:4200
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   };
   
