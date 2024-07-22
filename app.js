2db2f9d52ddebccbc999ece2674cefb4c378d7fa
yatheendrasai1 <yatheendrasai1@gmail.com>
2024-07-23 00:22:32 +0530

    BEBANTU-8: Updated the cors headers code

--- app.js
+++ app.js
@@ -25,36 +25,13 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
-
-
-// const corsOptions = {
-//     origin: 'https://bantu-listen.vercel.app', // Allow requests from localhost:4200
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-//   };
-  
-
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://bantu-listen.vercel.app');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.options('*', cors(corsOptions)); // Enable pre-flight for all routes
 
 app.use('/api', userRoutes);
 app.use('/api', notificationLogRoutes); 
